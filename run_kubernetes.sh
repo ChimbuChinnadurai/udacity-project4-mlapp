89158e39c010333813f1f1db4d001eb1c08260e3
Chimbu Chinnadurai <chimbu.chinnadurai@waitrose.co.uk>
2020-07-11 18:13:34 +0100

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+
+dockerpath=simbu1290/mlapp:latest
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+
+kubectl run mlapp --image=$dockerpath --port=80 --restart=Never
+
+
+# Step 3:
+# List kubernetes pods
+
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+
+kubectl port-forward pod/mlapp 8000:80
